f0f8fff526f46cc794265708e6368cad0495d074
Isaí Alejandro <isaialejandro26@gmail.com>
2022-01-17 19:09:42 -0600

    Static Main config.
    Fixing staticfiles config.

--- atlas_storage/settings.py
+++ atlas_storage/settings.py
@@ -137,15 +137,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# Location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# Location where you will store your static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # For media files:
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
